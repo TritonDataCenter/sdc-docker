ed6ba209a890834da0626ddbb3b417d6c401aae1
Trent Mick <trentm@gmail.com>
2014-10-21 06:55:48 +0000

    'make release' to build a shar installer; 'make publish' quick hack

--- Makefile
+++ Makefile
@@ -17,12 +17,13 @@ JSL_FILES_NODE	 = $(JS_FILES)
 JSSTYLE_FILES	 = $(JS_FILES)
 JSSTYLE_FLAGS	 = -f tools/jsstyle.conf
 SMF_MANIFESTS_IN = smf/manifests/docker.xml.in
-CLEAN_FILES += ./node_modules
+CLEAN_FILES += ./node_modules build/docker.js-*.sh
 
 NODE_PREBUILT_VERSION=v0.10.32
 ifeq ($(shell uname -s),SunOS)
-	NODE_PREBUILT_TAG=zone
-	NODE_PREBUILT_IMAGE=de411e86-548d-11e4-a4b7-3bb60478632a
+	NODE_PREBUILT_TAG=gz
+	# Get a 'gz' build for zone de411e86-548d-11e4-a4b7-3bb60478632a (base).
+	NODE_PREBUILT_IMAGE=fd2cc906-8938-11e3-beab-4359c665ac99
 endif
 
 
@@ -35,8 +36,12 @@ else
 endif
 include ./tools/mk/Makefile.smf.defs
 
+VERSION=$(shell json -f $(TOP)/package.json version)
+RELEASE_PKG=build/docker.js-$(VERSION).sh
 
-
+MANTA_URL=https://us-east.manta.joyent.com
+MLN=MANTA_URL=$(MANTA_URL) mln
+MPUT=MANTA_URL=$(MANTA_URL) mput
 
 #
 # Targets
@@ -45,6 +50,15 @@ include ./tools/mk/Makefile.smf.defs
 all: $(SMF_MANIFESTS) | $(NPM_EXEC)
 	$(NPM) install
 
+.PHONY: release
+#release: $(RELEASE_PKG)
+release $(RELEASE_PKG):
+	./tools/mk-shar -o $(TOP)/build -s $(VERSION)
+
+.PHONY: publish
+publish: $(RELEASE_PKG)
+	$(MPUT) -f $(RELEASE_PKG) /Joyent_Dev/stor/tmp/docker.js-$(VERSION).sh
+	$(MLN) /Joyent_Dev/stor/tmp/docker.js-$(VERSION).sh /Joyent_Dev/stor/tmp/docker.js.sh
 
 include ./tools/mk/Makefile.deps
 ifeq ($(shell uname -s),SunOS)
---
+++ tools/install-docker.js.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
+#
+# This is the script included in docker.js shars to handle
+# the docker.js install/upgrade on a headnode GZ.
+#
+# Usage:
+#   install-docker.js.sh    # in the extracted shar dir
+#
+
+if [[ -n "$TRACE" ]]; then
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+set -o errexit
+set -o pipefail
+
+
+DESTDIR=/opt/smartdc/docker.js
+NEWDIR=$DESTDIR.new
+OLDDIR=$DESTDIR.old
+
+
+#---- support stuff
+
+function fatal
+{
+    echo "$0: fatal error: $*" >&2
+    exit 1
+}
+
+function restore_old_on_error
+{
+    [[ $1 -ne 0 ]] || exit 0
+
+    if [[ -d $OLDDIR ]]; then
+        echo "$0: restoring $DESTDIR from $OLDDIR"
+        rm -rf $DESTDIR
+        mv $OLDDIR $DESTDIR
+    fi
+
+    fatal "$0: error exit status $1" >&2
+}
+
+
+#---- mainline
+
+# Sanity checks.
+[[ "$(zonename)" == "global" ]] || fatal "not running in global zone"
+[[ "$(sysinfo | json "Boot Parameters.headnode")" == "true" ]] \
+    || fatal "not running on the headnode"
+[[ -f "./etc/buildstamp" ]] || fatal "missing './etc/buildstamp'"
+
+[[ -d $OLDDIR ]] && rm -rf $OLDDIR
+[[ -d $NEWDIR ]] && rm -rf $NEWDIR
+
+trap 'restore_old_on_error $?' EXIT
+
+cp -PR ./ $NEWDIR
+rm $NEWDIR/install-docker.js.sh
+rm -rf $NEWDIR/.temp_bin
+
+# Move the old out of the way, swap in the new.
+if [[ -d $DESTDIR ]]; then
+    mv $DESTDIR $OLDDIR
+fi
+mv $NEWDIR $DESTDIR
+
+# Import the docker engine service and gracefully start it.
+echo "Importing and starting docker service"
+cp $DESTDIR/smf/manifests/docker.xml /var/svc/manifest/site/docker.xml
+svccfg import /var/svc/manifest/site/docker.xml
+if [[ "$(svcs -Ho state docker)" == "maintenance" ]]; then
+    svcadm clear docker
+fi
+
+[[ -d $OLDDIR ]] && rm -rf $OLDDIR
+
+echo "Successfully upgraded to docker.js $(cat $DESTDIR/etc/buildstamp)"
+exit 0
