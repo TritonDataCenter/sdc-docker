18495545d2b685f2cf4ecff7a432962a713a7a58
Trent Mick <trentm@gmail.com>
2014-11-06 15:58:36 -0800

    DOCKER-31 Add MPL header

--- tools/DOCKER_HOST-from-hn.sh
+++ tools/DOCKER_HOST-from-hn.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
+#
 # Dump 'export DOCKER_HOST=...' as appropriate for the sdc-docker running
 # on the given headnode.
 #
