1b3c906ab70cfbe6a8f55a6814b18bd3099aa546
Todd Whiteman <todd.whiteman@joyent.com>
2015-06-23 12:24:08 -0700

    DOCKER-359: add support for labels - container create and inspect

---
+++ test/integration/labels.test.js
@@ -0,0 +1,72 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2015, Joyent, Inc.
+ */
+
+/*
+ * Integration tests for docker labels.
+ */
+
+var cli = require('../lib/cli');
+var vm = require('../lib/vm');
+var test = require('tape');
+var vasync = require('vasync');
+
+
+
+// --- Globals
+
+
+var CLIENTS = {};
+var CONTAINER_PREFIX = 'sdcdockertest_labels_';
+
+
+// --- Helpers
+
+
+// --- Tests
+
+
+test('setup', function (tt) {
+
+    tt.test('DockerEnv: alice init', cli.init);
+
+    tt.test('vmapi client', vm.init);
+});
+
+
+test('labels', function (tt) {
+
+    var containerId;
+
+    tt.test('simple label', function (t) {
+        cli.run(t, { args: '-d --label foo=bar '
+                        + '--label "elem=something with a space" '
+                        + 'busybox sleep 3600' }, function (err, id)
+        {
+            t.ifErr(err, 'docker run --label foo=bar busybox');
+            containerId = id;
+            t.end();
+        });
+    });
+
+
+    tt.test('simple label check', function (t) {
+        cli.inspect(t, {
+            id: containerId,
+            partialExp: {
+                Config: {
+                    Labels: {
+                        'foo': 'bar',
+                        'elem': 'something with a space'
+                    }
+                }
+            }
+        });
+    });
+});
