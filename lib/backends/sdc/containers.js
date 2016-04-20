TritonDataCenter/sdc-docker

commit f3662408dcde55a7f13b2d4e8dbf92b14432ac62

DOCKER-630: Allow user to pass locality hint/restriction in docker run/create (swarm-iness)
Reviewed-By: Josh Wilsdon <jwilsdon@joyent.com>
Reviewed-By: Todd Whiteman <todd.whiteman@joyent.com>

lib/backends/sdc/containers.js
@@ -29,6 +29,7 @@ var triton_tags = require('triton-tags');
 var vasync = require('vasync');
 var VMAPI = require('sdc-clients').VMAPI;
 
+var affinity = require('./affinity');
 var common = require('../../../lib/common');
 var errors = require('../../../lib/errors');
 var images = require('./images');
@@ -1376,7 +1377,9 @@ function listDockerVms(opts, callback) {
     assert.optionalBool(opts.one, 'opts.one'); // docker stop/start
     assert.func(callback, 'callback');
 
-    var params = {};
+    var params = {
+        fields: '*'
+    };
 
     // query for one vm or all vms must include running/stopped
     if (opts.one || opts.all) {
@@ -1402,9 +1405,7 @@ function listDockerVms(opts, callback) {
         });
     }
 
-    // XXX should we need headers?
     opts.vmapi.listVms(params, {
-        fields: '*',
         headers: {'x-request-id': opts.req_id}
     }, function _listVmsCb(err, vms, _req, _res) {
         if (err) {
@@ -1970,6 +1971,28 @@ function buildVmPayload(opts, container, callback) {
             });
         },
 
+        /*
+         * Determine locality hints from Docker Swarm's syntaxes for
+         * container "affinities".
+         */
+        function addLocalityHintsFromAffinities(_, cb) {
+            affinity.localityFromContainer({
+                log: opts.log,
+                vmapi: opts.vmapi,
+                ownerUuid: opts.account.uuid,
+                container: container
+            }, function (err, locality) {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                if (locality) {
+                    payload.locality = locality;
+                }
+                cb();
+            });
+        },
+
         function addImageData(_, cb) {
             payload.image_uuid = opts.image.image_uuid;
 
@@ -2197,6 +2220,7 @@ function getContainerCount(opts, callback) {
 
     var vmapi = getVmapiClient(this.config.vmapi);
     var params = {
+        fields: '*',
         predicate: JSON.stringify({
             and: [
                 { eq: [ 'docker', true ] },
@@ -2212,7 +2236,6 @@ function getContainerCount(opts, callback) {
     };
 
     vmapi.countVms(params, {
-        fields: '*',
         headers: {'x-request-id': opts.req_id}
     }, function _countVmsCb(err, vmcount, _req, _res) {
         if (err) {

lib/errors.js
@@ -312,6 +312,36 @@ AmbiguousDockerImageIdError.description =
     + 'same Docker id is pulled from multiple registries';
 
 
+/*
+ * An error to return when a given Docker container ID prefix is required to
+ * identify a single docker container uniquely, but instead matches more
+ * than one. Optionally pass in the matching `ids` to have those included
+ * in the error message.
+ */
+function AmbiguousDockerContainerIdPrefixError(idPrefix, ids) {
+    assert.string(idPrefix, 'idPrefix');
+    assert.optionalArrayOfString(ids, 'ids');
+
+    var message = fmt('multiple container IDs match prefix "%s"', idPrefix);
+    if (ids && ids.length > 0) {
+        message += ': ' + ids.join(', ');
+    }
+    _DockerBaseError.call(this, {
+        restCode: this.constructor.restCode,
+        statusCode: this.constructor.statusCode,
+        message: message
+    });
+}
+util.inherits(AmbiguousDockerContainerIdPrefixError, _DockerBaseError);
+AmbiguousDockerContainerIdPrefixError.prototype.name
+    = 'AmbiguousDockerContainerIdPrefixError';
+AmbiguousDockerContainerIdPrefixError.restCode
+    = 'AmbiguousDockerContainerIdPrefix';
+AmbiguousDockerContainerIdPrefixError.statusCode = 404;
+AmbiguousDockerContainerIdPrefixError.description =
+    'A Docker container id prefix matches more than one container.';
+
+
 /**
  * An error used to expose the error from a node-sdc-clients API request.
  *
@@ -652,6 +682,8 @@ module.exports = {
     ServiceDegradedError: ServiceDegradedError,
     DockerContainerNotRunningError: DockerContainerNotRunningError,
     AmbiguousDockerImageIdError: AmbiguousDockerImageIdError,
+    AmbiguousDockerContainerIdPrefixError:
+        AmbiguousDockerContainerIdPrefixError,
     FileNotFoundError: FileNotFoundError,
     PathNotDirectoryError: PathNotDirectoryError,
 

commit c5094ce233b51b169c7814b7ea5855932ec1f748

DOCKER-804: Docker ps no longer shows the status for exited containers
This was broken in commit f3662408dcde55a7f13b2d4e8dbf92b14432ac62 when
the 'fields=*' param used for VMAPI ListVms calls (in `listDockerVms`)
was innocently and naively fixed to actually be passed to VMAPI ListVms
(as opposed to just being silently unused in sdc-clients/lib/vmapi.js's
somewhat confusing arguments). That then hit ZAPI-736.

lib/backends/sdc/containers.js
@@ -1377,9 +1377,7 @@ function listDockerVms(opts, callback) {
     assert.optionalBool(opts.one, 'opts.one'); // docker stop/start
     assert.func(callback, 'callback');
 
-    var params = {
-        fields: '*'
-    };
+    var params = {};
 
     // query for one vm or all vms must include running/stopped
     if (opts.one || opts.all) {
@@ -2220,7 +2218,6 @@ function getContainerCount(opts, callback) {
 
     var vmapi = getVmapiClient(this.config.vmapi);
     var params = {
-        fields: '*',
         predicate: JSON.stringify({
             and: [
                 { eq: [ 'docker', true ] },