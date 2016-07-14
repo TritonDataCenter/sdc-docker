4c23a2aa8e489c3545f74f04f15e36a2e39d4c31
Matt Smillie <matt.smillie@joyent.com>
2016-07-13 23:14:11 -0700

    DOCKER-585 implements run --net (fixup to run all create tests)

--- test/integration/api-create.test.js
+++ test/integration/api-create.test.js
@@ -102,248 +102,248 @@ test('setup', function (tt) {
         });
     });
 
-    // tt.test('pull nginx image', function (t) {
-    //     var url = '/images/create?fromImage=nginx%3Alatest';
-    //     DOCKER_ALICE.post(url, function (err, req, res) {
-    //         t.error(err, 'should be no error posting image create request');
-    //         t.end();
-    //     });
-    // });
+    tt.test('pull nginx image', function (t) {
+        var url = '/images/create?fromImage=nginx%3Alatest';
+        DOCKER_ALICE.post(url, function (err, req, res) {
+            t.error(err, 'should be no error posting image create request');
+            t.end();
+        });
+    });
 
 });
 
-// test('api: create with non-string label values (DOCKER-737)', function (t) {
-//     var payload = {
-//         // Boilerplate
-//         'Hostname': '',
-//         'Domainname': '',
-//         'User': '',
-//         'Memory': 0,
-//         'MemorySwap': 0,
-//         'CpuShares': 0,
-//         'Cpuset': '',
-//         'AttachStdin': false,
-//         'AttachStdout': false,
-//         'AttachStderr': false,
-//         'PortSpecs': null,
-//         'ExposedPorts': {},
-//         'Tty': false,
-//         'OpenStdin': false,
-//         'StdinOnce': false,
-//         'Env': [],
-//         'Cmd': null,
-//         'Image': 'nginx',
-//         'Volumes': {},
-//         'WorkingDir': '',
-//         'Entrypoint': null,
-//         'NetworkDisabled': false,
-//         'OnBuild': null,
-//         'SecurityOpt': null,
-//         'HostConfig': {
-//             'Binds': null,
-//             'ContainerIDFile': '',
-//             'LxcConf': [],
-//             'Privileged': false,
-//             'PortBindings': {},
-//             'Links': null,
-//             'PublishAllPorts': false,
-//             'Dns': null,
-//             'DnsSearch': null,
-//             'ExtraHosts': null,
-//             'VolumesFrom': null,
-//             'Devices': [],
-//             'NetworkMode': 'bridge',
-//             'CapAdd': null,
-//             'CapDrop': null,
-//             'RestartPolicy': {
-//                 'Name': '',
-//                 'MaximumRetryCount': 0
-//             }
-//         },
+test('api: create with non-string label values (DOCKER-737)', function (t) {
+    var payload = {
+        // Boilerplate
+        'Hostname': '',
+        'Domainname': '',
+        'User': '',
+        'Memory': 0,
+        'MemorySwap': 0,
+        'CpuShares': 0,
+        'Cpuset': '',
+        'AttachStdin': false,
+        'AttachStdout': false,
+        'AttachStderr': false,
+        'PortSpecs': null,
+        'ExposedPorts': {},
+        'Tty': false,
+        'OpenStdin': false,
+        'StdinOnce': false,
+        'Env': [],
+        'Cmd': null,
+        'Image': 'nginx',
+        'Volumes': {},
+        'WorkingDir': '',
+        'Entrypoint': null,
+        'NetworkDisabled': false,
+        'OnBuild': null,
+        'SecurityOpt': null,
+        'HostConfig': {
+            'Binds': null,
+            'ContainerIDFile': '',
+            'LxcConf': [],
+            'Privileged': false,
+            'PortBindings': {},
+            'Links': null,
+            'PublishAllPorts': false,
+            'Dns': null,
+            'DnsSearch': null,
+            'ExtraHosts': null,
+            'VolumesFrom': null,
+            'Devices': [],
+            'NetworkMode': 'bridge',
+            'CapAdd': null,
+            'CapDrop': null,
+            'RestartPolicy': {
+                'Name': '',
+                'MaximumRetryCount': 0
+            }
+        },
 
-//         // The interesting data we are actually testing in this test case:
-//         Labels: {
-//             foo: true,
-//             anum: 3.14
-//         }
-//     };
-//     var apiVersion = 'v' + constants.API_VERSION;
-//     DOCKER_ALICE.post(
-//         '/' + apiVersion + '/containers/create',
-//         payload,
-//         function (err, req, res) {
-//             t.ok(err, 'expect err from container create');
-//             /* JSSTYLED */
-//             var errRe = /^\(Validation\) invalid labels: label "foo" value is not a string: true; label "anum" value is not a string: 3.14/;
-//             t.ok(errRe.exec(err.message), format('err.message matches %s: %j',
-//                 errRe, err.message));
-//             t.end();
-//         });
-// });
-
-
-// test('api: create', function (tt) {
-
-//     var created;
-
-//     tt.test('docker create', function (t) {
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_ALICE,
-//             test: t
-//         }, oncreate);
-
-//         function oncreate(err, result) {
-//             t.ifErr(err, 'create container');
-//             created = result;
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker rm', function (t) {
-//         DOCKER_ALICE.del('/containers/' + created.id, ondel);
-
-//         function ondel(err, res, req, body) {
-//             t.ifErr(err, 'rm container');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker create without approved_for_provisioning', function (t) {
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_BOB,
-//             // we expect errors here, so stub this out
-//             test: {
-//                 deepEqual: stub,
-//                 equal: stub,
-//                 error: stub,
-//                 ok: stub
-//             }
-//         }, oncreate);
-
-//         function oncreate(err, result) {
-//             t.ok(err, 'should not create without approved_for_provisioning');
-//             t.equal(err.statusCode, 403);
-
-//             var expected = BOB.login + ' does not have permission to pull or '
-//                 + 'provision';
-//             t.ok(err.message.match(expected));
-
-//             t.end();
-//         }
-
-//         function stub() {}
-//     });
-
-//     tt.test('docker create without memory override', function (t) {
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_ALICE,
-//             test: t
-//         }, oncreate);
-
-//         function oncreate(err, result) {
-//             t.ifErr(err, 'create container');
-//             created = result;
-//             t.equal(created.vm.ram, CONFIG.defaultMemory,
-//                     'VM with default memory specs should be created with '
-//                     + CONFIG.defaultMemory + ' MBs of RAM');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker rm', function (t) {
-//         DOCKER_ALICE.del('/containers/' + created.id, ondel);
-
-//         function ondel(err, res, req, body) {
-//             t.ifErr(err, 'rm container');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker create with 2GB memory', function (t) {
-//         var MEMORY_IN_MBS = CONFIG.defaultMemory * 2;
-//         var memory = MEMORY_IN_MBS * BYTES_IN_MB;
-
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_ALICE,
-//             test: t,
-//             extra: { 'HostConfig.Memory': memory }
-//         }, oncreate);
-
-//         function oncreate(err, result) {
-//             t.ifErr(err, 'create container');
-//             created = result;
-//             t.equal(created.vm.ram, MEMORY_IN_MBS,
-//                     'VM should be created with ' + MEMORY_IN_MBS
-//                     + 'MBs of RAM');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker rm', function (t) {
-//         DOCKER_ALICE.del('/containers/' + created.id, ondel);
-
-//         function ondel(err, res, req, body) {
-//             t.ifErr(err, 'rm container');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker create with invalid memory',
-//             function (t) {
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_ALICE,
-//             test: t,
-//             extra: { 'HostConfig.Memory': 'Foo' }
-//         }, oncreate);
-
-//         function oncreate(err, result) {
-//             t.ifErr(err, 'create container');
-//             created = result;
-//             t.equal(created.vm.ram, CONFIG.defaultMemory,
-//                     'VM should be created with ' + CONFIG.defaultMemory
-//                     + 'MBs of RAM');
-//             t.end();
-//         }
-//     });
-
-//     tt.test('docker rm', function (t) {
-//         DOCKER_ALICE.del('/containers/' + created.id, ondel);
-
-//         function ondel(err, res, req, body) {
-//             t.ifErr(err, 'rm container');
-//             t.end();
-//         }
-//     });
-// });
+        // The interesting data we are actually testing in this test case:
+        Labels: {
+            foo: true,
+            anum: 3.14
+        }
+    };
+    var apiVersion = 'v' + constants.API_VERSION;
+    DOCKER_ALICE.post(
+        '/' + apiVersion + '/containers/create',
+        payload,
+        function (err, req, res) {
+            t.ok(err, 'expect err from container create');
+            /* JSSTYLED */
+            var errRe = /^\(Validation\) invalid labels: label "foo" value is not a string: true; label "anum" value is not a string: 3.14/;
+            t.ok(errRe.exec(err.message), format('err.message matches %s: %j',
+                errRe, err.message));
+            t.end();
+        });
+});
 
 
-// test('api: create with env var that has no value (DOCKER-741)', function (tt) {
-//     tt.test('create empty-env-var container', function (t) {
-//         h.createDockerContainer({
-//             vmapiClient: VMAPI,
-//             dockerClient: DOCKER_ALICE,
-//             test: t,
-//             extra: { 'Env': ['ENV_NO_VALUE'] },
-//             start: true  // Will start the container after creating.
-//         }, oncreate);
+test('api: create', function (tt) {
 
-//         function oncreate(err, result) {
-//             t.ifErr(err, 'create empty-env-var container');
-//             t.equal(result.vm.state, 'running', 'Check container running');
-//             DOCKER_ALICE.del('/containers/' + result.id + '?force=1', ondelete);
-//         }
+    var created;
 
-//         function ondelete(err) {
-//             t.ifErr(err, 'delete empty-env-var container');
-//             t.end();
-//         }
-//     });
-// });
+    tt.test('docker create', function (t) {
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_ALICE,
+            test: t
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ifErr(err, 'create container');
+            created = result;
+            t.end();
+        }
+    });
+
+    tt.test('docker rm', function (t) {
+        DOCKER_ALICE.del('/containers/' + created.id, ondel);
+
+        function ondel(err, res, req, body) {
+            t.ifErr(err, 'rm container');
+            t.end();
+        }
+    });
+
+    tt.test('docker create without approved_for_provisioning', function (t) {
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_BOB,
+            // we expect errors here, so stub this out
+            test: {
+                deepEqual: stub,
+                equal: stub,
+                error: stub,
+                ok: stub
+            }
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ok(err, 'should not create without approved_for_provisioning');
+            t.equal(err.statusCode, 403);
+
+            var expected = BOB.login + ' does not have permission to pull or '
+                + 'provision';
+            t.ok(err.message.match(expected));
+
+            t.end();
+        }
+
+        function stub() {}
+    });
+
+    tt.test('docker create without memory override', function (t) {
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_ALICE,
+            test: t
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ifErr(err, 'create container');
+            created = result;
+            t.equal(created.vm.ram, CONFIG.defaultMemory,
+                    'VM with default memory specs should be created with '
+                    + CONFIG.defaultMemory + ' MBs of RAM');
+            t.end();
+        }
+    });
+
+    tt.test('docker rm', function (t) {
+        DOCKER_ALICE.del('/containers/' + created.id, ondel);
+
+        function ondel(err, res, req, body) {
+            t.ifErr(err, 'rm container');
+            t.end();
+        }
+    });
+
+    tt.test('docker create with 2GB memory', function (t) {
+        var MEMORY_IN_MBS = CONFIG.defaultMemory * 2;
+        var memory = MEMORY_IN_MBS * BYTES_IN_MB;
+
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_ALICE,
+            test: t,
+            extra: { 'HostConfig.Memory': memory }
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ifErr(err, 'create container');
+            created = result;
+            t.equal(created.vm.ram, MEMORY_IN_MBS,
+                    'VM should be created with ' + MEMORY_IN_MBS
+                    + 'MBs of RAM');
+            t.end();
+        }
+    });
+
+    tt.test('docker rm', function (t) {
+        DOCKER_ALICE.del('/containers/' + created.id, ondel);
+
+        function ondel(err, res, req, body) {
+            t.ifErr(err, 'rm container');
+            t.end();
+        }
+    });
+
+    tt.test('docker create with invalid memory',
+            function (t) {
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_ALICE,
+            test: t,
+            extra: { 'HostConfig.Memory': 'Foo' }
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ifErr(err, 'create container');
+            created = result;
+            t.equal(created.vm.ram, CONFIG.defaultMemory,
+                    'VM should be created with ' + CONFIG.defaultMemory
+                    + 'MBs of RAM');
+            t.end();
+        }
+    });
+
+    tt.test('docker rm', function (t) {
+        DOCKER_ALICE.del('/containers/' + created.id, ondel);
+
+        function ondel(err, res, req, body) {
+            t.ifErr(err, 'rm container');
+            t.end();
+        }
+    });
+});
+
+
+test('api: create with env var that has no value (DOCKER-741)', function (tt) {
+    tt.test('create empty-env-var container', function (t) {
+        h.createDockerContainer({
+            vmapiClient: VMAPI,
+            dockerClient: DOCKER_ALICE,
+            test: t,
+            extra: { 'Env': ['ENV_NO_VALUE'] },
+            start: true  // Will start the container after creating.
+        }, oncreate);
+
+        function oncreate(err, result) {
+            t.ifErr(err, 'create empty-env-var container');
+            t.equal(result.vm.state, 'running', 'Check container running');
+            DOCKER_ALICE.del('/containers/' + result.id + '?force=1', ondelete);
+        }
+
+        function ondelete(err) {
+            t.ifErr(err, 'delete empty-env-var container');
+            t.end();
+        }
+    });
+});
 
 test('ensure fabrics enabled', function (tt) {
     tt.test('fabric configuration', function (t) {
