1653c4f0fc7c52f9baa54ad396ac10df452802e0
Trent Mick <trentm@gmail.com>
2016-12-05 15:15:17 -0800

    DOCKER-932 docker sometimes returns success without completing pull DOCKER-980 "docker pull ..." auth error should yield a "image $name not found" error message Reviewed by: Todd Whiteman <todd.whiteman@joyent.com>

--- test/integration/cli-pull.test.js
+++ test/integration/cli-pull.test.js
@@ -19,77 +19,99 @@ var h = require('./helpers');
 var cli = require('../lib/cli');
 
 
-
 // --- Tests
-
 
 test('docker pull', function (tt) {
 
     tt.test('  setup: alice init', cli.init);
 
+
     /**
      * Check for reasonable error messages for some 'docker pull' failures.
      * Some related issues: DOCKER-639, DOCKER-689
      */
-    /*
-     * XXX: Skipped pending DOCKER-976.
+
+    /**
+     * Docker-docker 1.12:
+     *  $ docker pull no-such-repo
+     *  Using default tag: latest
+     *  Pulling repository docker.io/library/no-such-repo
+     *  Error: image library/no-such-repo not found
+     *  $ echo $?
+     *  1
+     *
+     * Note that Triton-docker won't have that "Pulling repository ..." line.
+     * In Docker-docker, that is from the docker v1 fallback pull attempt.
+     * I.e. cruft, IMO.
      */
-    tt.skip('  docker pull no-such-repo (error message)', function (t) {
+    tt.test('  docker pull no-such-repo', function (t) {
         cli.docker('pull no-such-repo', function (err, stdout, stderr) {
-            // JSSTYLED
-            // I.e. this error message: https://github.com/docker/distribution/blob/master/registry/api/errcode/register.go#L40
-            var unauthorizedMsgRe = /authentication required/;
+            t.ok(err, 'expect failed pull: ' + err);
 
-            /*
-             * Actually expect a zero exit status, because `docker pull`s
-             * JSON progress protocol doesn't handle communicating an error,
-             * AFAIK.
-             */
-            t.ifError(err, 'expected successful pull');
-
-            t.ok(/UNAUTHORIZED/.test(stdout), format(
-                'error code is "UNAUTHORIZED", from stdout: %j', stdout));
-
-            t.ok(unauthorizedMsgRe.test(stdout), format(
-                'error message matches %s, got %j', unauthorizedMsgRe, stdout));
+            t.ok(/^Error: image no-such-repo:latest not found/m.test(stderr),
+                format('stderr includes "Error: image $name not found", '
+                    + 'stdout=%j, stderr=%j', stdout, stderr));
+            t.ok(! /unauthorized/i.test(stderr), format('stderr does '
+                + '*not* contain "unauthorized", from stderr=%j', stderr));
 
             t.end();
         });
     });
 
-    /*
-     * XXX: Skipped pending DOCKER-976.
+    /* BEGIN JSSTYLED */
+    /**
+     * Docker-docker 1.12:
+     *  $ docker pull quay.io/no-such-user
+     *  Using default tag: latest
+     *  Error response from daemon: error parsing HTTP 404 response body: invalid character '<' looking for beginning of value: "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n<title>404 Not Found</title>\n<h1>Not Found</h1>\n<p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>\n"
+     *
+     * Triton-docker:
+     *  $ docker --tls pull quay.io/no-such-user
+     *  Using default tag: latest
+     *  Error: image quay.io/no-such-user:latest not found (71a0653b...)
+     *
+     * I think Triton's is an improvement.
      */
-    tt.skip('  docker pull quay.io/no-such-user (error message)', function (t) {
+    /* END JSSTYLED */
+    tt.test('  docker pull quay.io/no-such-user', function (t) {
         cli.docker('pull quay.io/no-such-user',
                 function (err, stdout, stderr) {
-            var match;
+            t.ok(err, 'expect failed pull: ' + err);
 
-            // expect zero exit status, see above
-            t.ifError(err, 'expected successful pull');
+            t.ok(/^Error: image quay.io\/no-such-user:latest not found/m
+                    .test(stderr),
+                format('stderr includes "Error: image $name not found", '
+                    + 'stdout=%j, stderr=%j', stdout, stderr));
+            t.ok(! /unauthorized/i.test(stderr), format('stderr does '
+                + '*not* contain "unauthorized", from stderr=%j', stderr));
 
-            // JSSTYLED
-            match = /Not Found \(404\) error from registry quay.io trying to pull no-such-user/.test(stdout);
-
-            t.ok(match, 'expected that error is "Not Found ..."'
-                + (match ? '' : format(', got: %j', stdout)));
             t.end();
         });
     });
 
-    tt.test('  docker pull nope.example.com/nope (error message)',
-            function (t) {
+    /* BEGIN JSSTYLED */
+    /*
+     * Docker-docker 1.12:
+     *  $ docker pull nope.example.com/nope
+     *  Using default tag: latest
+     *  Error response from daemon: Get https://nope.example.com/v1/_ping: dial tcp: lookup nope.example.com on 8.8.8.8:53: no such host
+     *
+     * Triton-docker:
+     *  $ docker --tls pull nope.example.com/nope
+     *  Using default tag: latest
+     *  Error pulling image: (ENOTFOUND) nope.example.com host not found (...)
+     */
+    /* END JSSTYLED */
+    tt.test('  docker pull nope.example.com/nope', function (t) {
         cli.docker('pull nope.example.com/nope',
             function (err, stdout, stderr) {
-            var notFound;
+            t.ok(err, 'expect failed pull: ' + err);
 
-            // expect zero exit status, see above
-            t.ifError(err, 'expected successful pull');
+            /* JSSTYLED */
+            var pat = /^Error pulling image: \(ENOTFOUND\) nope.example.com host not found/m;
+            t.ok(pat.test(stderr), format('stderr matches %s, stderr=%j',
+                pat, stderr));
 
-            notFound
-                = /\(ENOTFOUND\) nope.example.com host not found/.test(stdout);
-            t.ok(notFound, 'error is "ENOTFOUND"'
-                + (notFound ? '' : ', got: ' + stdout));
             t.end();
         });
     });
