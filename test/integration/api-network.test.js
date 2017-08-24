4bb802d3fd1f38f5aa30d77b0e0613d51009e981
Todd Whiteman <todd.whiteman@joyent.com>
2017-08-24 10:04:15 -0700

    allow vlan_id to be passed in for create network, add more tests

--- test/integration/api-network.test.js
+++ test/integration/api-network.test.js
@@ -13,6 +13,7 @@
  */
 
 var test = require('tape');
+var vasync = require('vasync');
 
 var h = require('./helpers');
 var testcommon = require('../lib/common');
@@ -74,7 +75,7 @@ test('setup', function (tt) {
 
 test('docker network ls', function (tt) {
     tt.test('list networks', function (t) {
-        DOCKER_ALICE.get('/networks', function (err, res, req, networks) {
+        DOCKER_ALICE.get('/networks', function (err, req, res, networks) {
             t.ifErr(err, 'check /networks err');
             t.ok(Array.isArray(networks), 'check /networks returned an array');
             t.ok(networks.length >= 1, 'array contains at least one network');
@@ -122,7 +123,7 @@ test('docker network ls', function (tt) {
 test('docker network inspect', function (tt) {
     tt.test('inspect external network', function (t) {
         DOCKER_ALICE.get('/networks/external',
-            function (err, res, req, net)
+            function (err, req, res, net)
         {
             t.ifErr(err, 'check /networks/external err');
             t.equal(net.Driver, 'Triton', 'checking net.Driver');
@@ -158,7 +159,7 @@ test('docker network create', function (tt) {
         };
 
         DOCKER_ALICE.post('/networks/create', networkPayload,
-            function (err, res, req, result) {
+            function (err, req, res, result) {
                 t.ifErr(err, 'check /networks/create err');
                 if (err) {
                     t.end();
@@ -167,11 +168,215 @@ test('docker network create', function (tt) {
                 t.ok(result, 'check for result');
                 t.ok(result.Id, 'check for result.Id');
                 DOCKER_ALICE.del('/networks/' + result.Id,
-                    function (err2) {
-                        t.ifErr(err2, 'delete /networks/ err');
+                    function (dErr) {
+                        // Docker network rm is not implemented yet.
+                        t.ok(dErr, 'delete /networks/ should return an err');
+                        if (dErr) {
+                            t.ok(dErr.toString().indexOf('NotImplemented') >= 0,
+                                'Delete should return a NotImplemented error');
+                        }
                         t.end();
                     }
                 );
+            }
+        );
+    });
+});
+
+
+test('docker network create with existing vlan_id', function (tt) {
+    tt.test('create overlay network', function (t) {
+        var net1;
+        var net2;
+        var vlanIdStr;
+
+        vasync.pipeline({ funcs: [
+
+            function createNet1(_, next) {
+                var networkPayload = {
+                    Name: testcommon.makeNetworkName(NETWORK_NAME_PREFIX),
+                    Driver: 'overlay',
+                    IPAM: {
+                        Config: [
+                            {
+                                Subnet: '10.0.13.0/24'
+                            }
+                        ]
+                    }
+                };
+
+                DOCKER_ALICE.post('/networks/create', networkPayload,
+                    function (err, req, res, result) {
+                        t.ifErr(err, 'check /networks/create err');
+                        if (err) {
+                            next(err);
+                            return;
+                        }
+                        t.ok(result, 'check for result');
+                        t.ok(result.Id, 'check for result.Id');
+                        if (result.Id) {
+                            net1 = result;
+                        }
+                        next();
+                    }
+                );
+            },
+
+            function inspectNet1(_, next) {
+                DOCKER_ALICE.get('/networks/' + net1.Id,
+                    function (err, req, res, net) {
+                        t.ifErr(err, 'check /networks/net1 inspect err');
+                        if (err) {
+                            next(err);
+                            return;
+                        }
+                        t.equal(net.Driver, 'Triton', 'check inspect result');
+                        vlanIdStr = net.Labels
+                            && net.Labels['triton.network.vlan_id'];
+                        t.ok(vlanIdStr, 'got vlan_id from network.Labels');
+                        // End the test if no vlanIdStr was received.
+                        next(vlanIdStr ? null : (new Error('No vlan_id')));
+                    }
+                );
+            },
+
+            function createNet2(_, next) {
+                var networkPayload = {
+                    Name: testcommon.makeNetworkName(NETWORK_NAME_PREFIX),
+                    Driver: 'overlay',
+                    IPAM: {
+                        Config: [
+                            {
+                                Subnet: '10.0.14.0/24'
+                            }
+                        ]
+                    },
+                    Labels: {
+                        'triton.network.vlan_id': vlanIdStr
+                    }
+                };
+
+                DOCKER_ALICE.post('/networks/create', networkPayload,
+                    function (err, req, res, result) {
+                        t.ifErr(err, 'check /networks/create net2 err');
+                        if (err) {
+                            next(err);
+                            return;
+                        }
+                        t.ok(result, 'check for result');
+                        t.ok(result.Id, 'check for result.Id');
+                        if (result.Id) {
+                            net2 = result;
+                        }
+                        next();
+                    }
+                );
+            },
+
+            function deleteNet1(_, next) {
+                DOCKER_ALICE.del('/networks/' + net1.Id,
+                    function (err) {
+                        // Docker network rm is not implemented yet.
+                        t.ok(err, 'delete /networks/ should return an err');
+                        if (err) {
+                            t.ok(err.toString().indexOf('NotImplemented') >= 0,
+                                'Delete should return a NotImplemented error');
+                        }
+                        next();
+                    }
+                );
+            },
+
+            function deleteNet2(_, next) {
+                DOCKER_ALICE.del('/networks/' + net2.Id,
+                    function (err) {
+                        // Docker network rm is not implemented yet.
+                        t.ok(err, 'delete /networks/ should return an err');
+                        if (err) {
+                            t.ok(err.toString().indexOf('NotImplemented') >= 0,
+                                'Delete should return a NotImplemented error');
+                        }
+                        next();
+                    }
+                );
+            },
+
+            // The vlan should be cleaned up (it's unused) after deleting net2,
+            // so trying to create another network on the same vlan_id should
+            // fail (as the vlan_id is no longer valid - as it got deleted).
+            function createNetBadVlanId(_, next) {
+                t.skip('createNetBadVlanId depends upon network rm');
+                t.end();
+                return;
+
+                // var networkPayload = {
+                //     Name: testcommon.makeNetworkName(NETWORK_NAME_PREFIX),
+                //     Driver: 'overlay',
+                //     IPAM: {
+                //         Config: [
+                //             {
+                //                 Subnet: '10.0.15.0/24'
+                //             }
+                //         ]
+                //     },
+                //     Labels: {
+                //         'triton.network.vlan_id': vlanIdStr
+                //     }
+                // };
+
+                // DOCKER_ALICE.post('/networks/create', networkPayload,
+                //     function (err, req, res, result) {
+                //         t.ok(err,
+                //             'post /networks/create should return an err');
+                //         if (err) {
+                //             t.ok(err.toString().indexOf('Not found') >= 0,
+                //                 'Create should return a NotFound error');
+                //         }
+                //         next();
+                //     }
+                // );
+            }
+        ]}, function () {
+            t.end();
+        });
+    });
+});
+
+
+test('docker network create bogus vlan_id', function (tt) {
+    tt.test('create overlay network', function (t) {
+        var networkPayload = {
+            Name: testcommon.makeNetworkName(NETWORK_NAME_PREFIX),
+            Driver: 'overlay',
+            IPAM: {
+                Config: [
+                    {
+                        Subnet: '10.0.16.0/24'
+                    }
+                ]
+            },
+            Labels: {
+                'triton.network.vlan_id': '102938292'
+            }
+        };
+
+        DOCKER_ALICE.post('/networks/create', networkPayload,
+            function (err) {
+                var expectedErr;
+
+                t.ok(err, 'check /networks/create err');
+                if (err) {
+                    var es = err.toString();
+
+                    expectedErr = 'InvalidParameters';
+                    t.ok(es.indexOf(expectedErr) >= 0,
+                        'Create should error with "' + expectedErr + '"');
+
+                    expectedErr = 'VLAN ID must be a number between 0 and 4094';
+                    t.ok(es.indexOf(expectedErr) >= 0,
+                        'Create should error with "' + expectedErr + '"');
+                }
+                t.end();
             }
         );
     });
